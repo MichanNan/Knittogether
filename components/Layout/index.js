6aeb3e0ffe8e824ccb26c4fe63912ee547e72818
Nan Cao <caonanla@gmail.com>
2023-06-19 16:51:52 +0200

    delete comment

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -2,7 +2,6 @@ import styled from "styled-components";
 import Image from "next/image";
 
 export default function Navigation() {
-  // return <Nav>{homeSVG}</Nav>;
   return (
     <Nav>
       <Image
