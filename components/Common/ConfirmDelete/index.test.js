84c762f80d910f5e13afb15c55409d1f64ac1ad3
Nan Cao <caonanla@gmail.com>
2023-07-12 12:05:14 +0200

    test for component ConfirmDelete

--- components/Common/ConfirmDelete/index.test.js
+++ components/Common/ConfirmDelete/index.test.js
@@ -1,5 +1,8 @@
 import ConfirmDelete from ".";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+const onDeleteMock = jest.fn();
 
 test("render a delete button", () => {
   render(<ConfirmDelete />);
@@ -10,4 +13,10 @@ test("render a cancel button", () => {
   render(<ConfirmDelete />);
   const cancelButton = screen.getByText("Cancel");
   expect(cancelButton).toBeInTheDocument();
+});
+test("calls onDelete when Delete button is clocked", async () => {
+  render(<ConfirmDelete onDelete={onDeleteMock} />);
+  const deleteButton = screen.getByText("Delete");
+  await userEvent.click(deleteButton);
+  expect(onDeleteMock).toHaveBeenCalledTimes(1);
 });
